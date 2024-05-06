3b9494ebe669fdf63fcabee406ad8164082081ab
Mohammad Hamza Ibrahim <mi469@live.mdx.ac.uk>
2024-05-06 08:49:22 +0100

    Completed BankStatementPrinter: prints to console with correct formatting

--- src/BankStatementPrinter.js
+++ src/BankStatementPrinter.js
@@ -5,28 +5,47 @@ class BankStatementPrinter {
             throw new Error("Cannot create instance of abstract class");
     }
 
+    static valueFormat(transaction) {
+        return parseFloat((Math.round(transaction * 100) / 100)).toFixed(2);
+    }
+
+    // format on the printing of credit type in console
+    static creditPrint(transaction, balance) {
+        return `${transaction.getDate()} || ${this.valueFormat(transaction.getValue())} ||        || ${this.valueFormat(balance)}`;
+    }
+
+    // format on the printing of debit type in console
     static debitPrint(transaction, balance) {
-        const debitPrint = transaction.getValue();
-        const balancePrint = balance;
-        const datePrint = transaction.getDate();
-        return `${datePrint} ||         || ${debitPrint} || ${balancePrint}`;
+        return `${transaction.getDate()} ||         || ${this.valueFormat(transaction.getValue())} || ${this.valueFormat(balance)}`;
+    }
+
+    static printDetails(transaction, balance) {
+        if (transaction.getType() === "Credit") {
+            return console.log(this.creditPrint(transaction, balance));
+        }
+
+        return console.log(this.debitPrint(transaction, balance));
     }
 
     static printBankStatement(transactions, balance) {
-
         // reference:- https://stackoverflow.com/questions/5433313/convert-dd-mm-yyyy-to-mm-dd-yyyy-in-javascript
         function toDate(dateString) {
             let parts = dateString.split("/");
             return new Date(parts[2], parts[1] - 1, parts[0]);
         }
-
         // reference:- https://stackoverflow.com/questions/10123953/how-to-sort-an-object-array-by-date-property
         transactions.sort((a, b) => toDate(b.getDate()) - toDate(a.getDate()));
 
         console.log("date       || credit  || debit  || balance");
+        let currentBalance = balance;
         transactions.forEach((transaction) => {
-            console.log(this.debitPrint(transaction, balance));
-
+            this.printDetails(transaction, currentBalance);
+            if (transaction.getType() === "Credit") {
+                currentBalance -= parseFloat(transaction.getValue());
+            }
+            else {
+                currentBalance += parseFloat(transaction.getValue());
+            }
         });
     }
 }
--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import BankStatementPrinter from "./BankStatementPrinter.js";
 let user = new User("Hamza", "password");
 let bankAccount = new BankAccount();
 user.depositMoney(bankAccount, new Transaction("1000", "10/01/2012", "Credit"));
-user.depositMoney(bankAccount, new Transaction("500", "14/01/2012", "Debit"));
-user.depositMoney(bankAccount, new Transaction("2000", "13/01/2012", "Credit"));
 user.withdrawMoney(bankAccount, new Transaction("500", "14/01/2012", "Debit"));
+user.depositMoney(bankAccount, new Transaction("2000", "13/01/2012", "Credit"));
+// user.withdrawMoney(bankAccount, new Transaction("500", "14/01/2012", "Debit"));
 console.log("");
 
-console.log(BankStatementPrinter.printBankStatement(bankAccount.getTransaction()));
+console.log(BankStatementPrinter.printBankStatement(bankAccount.getTransaction(), bankAccount.getBalance()));
 
